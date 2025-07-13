bf00add1d4bbd186ac78f2ef3311731dd33c61a8
cerredz <422michaelcerreto@gmail.com>
2025-07-13 18:32:32 -0400

    slowly but steadily learning pytorch

---
+++ README.md
@@ -0,0 +1,3 @@
+building ensemble nn model, going to have 7 models: 6 trained on specialized data and 1 trained on generalized data
+
+also building architecture of a market simulator
--- src/train/personal_nn.py
+++ src/train/personal_nn.py
@@ -1,19 +1,46 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import pandas as pd
+import numpy as np
+import os
 
-class PersonalNet(nn.module):
-    def __init__(self):
-        super(PersonalNet, self).__init__()
-        # define nn structure
+# trained on name, gender, dob, and job
+class PersonalLinearNet(nn.Module):
+    def __init__(self, num_names: int, num_jobs: int, embedding_dim: int):
+        super(PersonalLinearNet, self).__init__()
+        self.name_embedding = nn.Embedding(num_names, embedding_dim)
+        self.job_embedding = nn.Embedding(num_jobs, embedding_dim)
+        
+        input_dim = embedding_dim * 2 + 1 + 3
+        self.model = nn.Sequential(
+            nn.Linear(input_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, 16),
+            nn.ReLU(),
+            nn.Linear(16, 4),
+            nn.ReLU(),
+            nn.Linear(4, 1)
+        )
 
-    def forward():
-        pass
+    def forward(self, name_idx, job_idx, gender, dob):
+        name_input = self.name_embedding(name_idx)
+        job_input  = self.name_embedding(job_idx)
+        
+        x = torch.cat([name_input, job_input, gender, dob.unsqueeze(1)], dim=1)
+        
+        return self.model(x)
+
+class PersonalDropoutNet(nn.Module):
+    pass
 
 def train():
-    pass
+    train_data_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "data", "Processed", "10mil", "personal_10000000.csv")
+    df = pd.read_csv(train_data_path)
+    print(df.head())
+
 
 
 
 if __name__ == "__main__":
-    pass
+    train()
